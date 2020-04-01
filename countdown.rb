1a41686db0e5f074f815e5c4aa9c109fc5ee01b9
Saida Davis <saihdavis@gmail.com>
2020-04-01 16:22:38 +0000

    Automatically backed up by Learn

---
+++ countdown.rb
@@ -0,0 +1,14 @@
+n = 10
+def countdown(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+  n -= 1  
+end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  while seconds >= 
+  sleep (0.5)
+  end
+end
